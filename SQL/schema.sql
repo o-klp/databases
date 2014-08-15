8b085de5bafe204b21d1479a29c6856a6b832fff
Hack Reactor Students <klp2012@gmail.com>
2014-08-15 16:19:36 -0700

    creates mysql database from schema.sql

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -2,8 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  UserID int NOT NULL AUTO_INCREMENT,
+  UserName varchar(20) NOT NULL UNIQUE,
+  PRIMARY KEY(UserID)
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  Roomname varchar(20) NOT NULL UNIQUE,
+  PRIMARY KEY(Roomname)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  MessageID int NOT NULL AUTO_INCREMENT,
+  UserID int NOT NULL,
+  Roomname varchar(20) NOT NULL,
+  MessageText blob NOT NULL,
+  PRIMARY KEY(MessageID),
+  FOREIGN KEY(UserID) REFERENCES users(UserID),
+  FOREIGN KEY(Roomname) REFERENCES rooms(Roomname)
 );
 
 /* Create other tables and define schemas for them here! */
